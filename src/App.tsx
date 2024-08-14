381274ddee15e17b75d24b0ad5002fec6e6c3d31
Aayush <aayushg2908@gmail.com>
2024-08-14 13:48:53 +0530

    Alai Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,64 @@
+import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
 import TldrawComponent from "./TldrawComponent";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "./components/ui/dialog";
+import { Input } from "./components/ui/input";
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [temp, setTemp] = useState(0);
+  const [noOfTimelines, setNoOfTimelines] = useState(0);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "c") {
+        setIsOpen(true);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setNoOfTimelines(temp);
+    setIsOpen(false);
+  };
+
   return (
-    <div style={{ position: "fixed", inset: 0 }}>
-      <h1>Tldraw Canvas:</h1>
-      <TldrawComponent />
-    </div>
+    <>
+      <nav className="flex items-center justify-between px-2 h-[50px]">
+        <h1 className="text-2xl font-bold">TLDraw Canvas:</h1>
+        <Button onClick={() => setIsOpen(true)} size="sm">
+          Create Timeline
+        </Button>
+      </nav>
+      <TldrawComponent noOfTimelines={noOfTimelines} />
+      <Dialog open={isOpen} onOpenChange={() => setIsOpen(false)}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create Timeline</DialogTitle>
+          </DialogHeader>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <Input
+              value={temp}
+              onChange={(e) => setTemp(+e.target.value)}
+              placeholder="No. of timelines you want to create"
+              type="number"
+            />
+            <Button type="submit" className="w-full">
+              Generate
+            </Button>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
--- src/TldrawComponent.tsx
+++ src/TldrawComponent.tsx
@@ -1,28 +1,115 @@
-import { Tldraw, createShapeId } from "tldraw";
+import { useEffect } from "react";
+import { TLShapeId, Tldraw, createShapeId, useEditor } from "tldraw";
 import "tldraw/tldraw.css";
 
-export default function TldrawComponent() {
+export default function TldrawComponent({
+  noOfTimelines,
+}: {
+  noOfTimelines: number;
+}) {
   return (
-    <div style={{ position: "fixed", width: "50vh", height: "50vh" }}>
-      <Tldraw
-        hideUi={true}
-        onMount={(editor) => {
-          const helloWorldShapeId = createShapeId();
-          editor.createShape({
-            id: helloWorldShapeId,
-            type: "text",
-            x: 200,
-            y: 200,
-            props: {
-              text: "Hello world!",
-            },
-          });
-
-          return () => {
-            editor.deleteShape(helloWorldShapeId);
-          };
-        }}
-      />
+    <div style={{ position: "fixed", width: "100vw", height: "100vh" }}>
+      <Tldraw hideUi>
+        <Timeline noOfTimelines={noOfTimelines} />
+      </Tldraw>
     </div>
   );
+}
+
+function Timeline({ noOfTimelines }: { noOfTimelines: number }) {
+  const editor = useEditor();
+  const ids: TLShapeId[] = [];
+
+  useEffect(() => {
+    if (noOfTimelines === 0) {
+      const initialId = createShapeId();
+      ids.push(initialId);
+      editor.createShape({
+        id: initialId,
+        type: "geo",
+        x: 500,
+        y: 200,
+        props: {
+          text: "Click on Create Timeline to add a timeline!",
+          w: 400,
+        },
+      });
+    } else {
+      const timelineAxisId = createShapeId();
+      ids.push(timelineAxisId);
+      editor.createShape({
+        id: timelineAxisId,
+        type: "line",
+        x: 500,
+        y: 300,
+        props: {
+          size: "l",
+          points: {
+            start: { id: "1", index: timelineAxisId, x: 0, y: 0 },
+            end: {
+              id: "2",
+              index: timelineAxisId,
+              x: noOfTimelines * 100,
+              y: 0,
+            },
+          },
+        },
+      });
+
+      for (let i = 0; i < noOfTimelines; i++) {
+        const timelineId = createShapeId();
+        ids.push(timelineId);
+        editor.createShape({
+          id: timelineId,
+          type: "line",
+          x: 500,
+          y: 300,
+          props: {
+            size: "m",
+            points: {
+              start: { id: "1", index: timelineAxisId, x: i * 100 + 50, y: 0 },
+              end: {
+                id: "2",
+                index: timelineAxisId,
+                x: i * 100 + 50,
+                y: i % 2 === 0 ? -50 : 50,
+              },
+            },
+          },
+        });
+
+        const timelineHeadingId = createShapeId();
+        ids.push(timelineHeadingId);
+        editor.createShape({
+          id: timelineHeadingId,
+          type: "text",
+          x: i * 100 + 500,
+          y: i % 2 === 0 ? 180 : 360,
+          props: {
+            text: `Subheading ${i + 1}`,
+            size: "s",
+          },
+        });
+
+        const timelineDescriptionId = createShapeId();
+        ids.push(timelineDescriptionId);
+        editor.createShape({
+          id: timelineDescriptionId,
+          type: "text",
+          x: i * 100 + 500,
+          y: i % 2 === 0 ? 210 : 390,
+          props: {
+            text: `Description ${i + 1}`,
+            size: "s",
+          },
+        });
+      }
+    }
+
+    return () => {
+      editor.deleteShapes(ids);
+    };
+  }, [noOfTimelines]);
+
+  return null;
 }
